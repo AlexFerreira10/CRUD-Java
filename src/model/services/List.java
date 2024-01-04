71f6de9f0fe49869cb27cc9867859891d5873c7c
AlexFerreira10 <correaferreiraalex@gmail.com>
2024-01-04 18:35:11 -0300

    completes the addition functions in the list

--- src/model/services/List.java
+++ src/model/services/List.java
@@ -3,20 +3,21 @@ package model.services;
 import java.util.Scanner;
 
 import model.entities.Node;
+import model.exceptions.DomainException;
 
 public class List {
 	private Node startNode;
 	private int counter;
 	private Scanner sc;
-	
+
 	public List() {
 	}
-	
+
 	public List(Scanner sc) {
 		super();
 		this.sc = sc;
 	}
-	
+
 	public List(Node startNode, int counter, Scanner sc) {
 		super();
 		this.startNode = startNode;
@@ -27,11 +28,11 @@ public class List {
 	public Node getStartNode() {
 		return startNode;
 	}
+
 //I guess that won'nt use it
-/*	public void setStartNode(Node startNode) {
-		this.startNode = startNode;
-	}
-*/
+	/*
+	 * public void setStartNode(Node startNode) { this.startNode = startNode; }
+	 */
 	public int getCounter() {
 		return counter;
 	}
@@ -39,44 +40,68 @@ public class List {
 	public void setCounter(int counter) {
 		this.counter = counter;
 	}
-	
+
 	public void addBeginning(Node node) {
-		/*Easy mode
-		//Case 1: List is empty
-		if(startNode == null) {
-			startNode = node;
-			node.setNextNode(null);
-		}
-		//Case 2: In list there is only or there are several node
-		else {
-			Node aux = startNode;
-			startNode = node;
-			node.setNextNode(aux);
-		} */
-		
-		//Optimized mode
+		/*
+		 * Easy mode //Case 1: List is empty if(startNode == null) { startNode = node;
+		 * node.setNextNode(null); } //Case 2: In list there is only or there are
+		 * several node else { Node aux = startNode; startNode = node;
+		 * node.setNextNode(aux); }
+		 */
+
+		// Optimized mode
 		node.setNextNode(startNode);
 		startNode = node;
 		counter++;
 	}
-	
-	public void print() {
+
+	public void addMiddle(Node node) throws DomainException {
 		Node aux = startNode;
-		while(aux != null){
-			
-			System.out.println(aux);
-			aux = aux.getNextNode();
+		if (startNode != null && startNode.getNextNode() != null) {
+
+			for (int i = 1; i < (counter / 2); i++) {
+				aux = aux.getNextNode();
+			}
+
+			node.setNextNode(aux.getNextNode());
+			aux.setNextNode(node);
+		} else {
+			throw new DomainException("There aren't suficient nodes in the list! ");
 		}
-		
-		System.out.println("Quantify of node: " + counter);
+		counter++;
 	}
-	
-	
+
+	public void addTheEnd(Node node) throws DomainException {
+		Node aux = startNode;
+		if (startNode != null) {
+			while (aux.getNextNode() != null) {
+				aux = aux.getNextNode();
+			}
+			aux.setNextNode(node);
+			node.setNextNode(null);
+		} else {
+			throw new DomainException("There aren't suficient nodes in the list! ");
+		}
+		counter++;
+	}
+
+	public void print() throws DomainException {
+		Node aux = startNode;
+		if (startNode != null) {
+			while (aux != null) {
+				System.out.println(aux);
+				aux = aux.getNextNode();
+			}
+			System.out.println("Quantify of node: " + counter);
+		} else {
+			throw new DomainException("There aren't suficient nodes in the list! ");
+		}
+	}
+
 	public void menu() {
 		int opcao = 0;
-		
-		do{
-			
+
+		do {
 			System.out.println("-------- List --------");
 			StringBuilder sb = new StringBuilder();
 			sb.append("[ 1 ] Insert (Begnning)\n");
@@ -96,60 +121,67 @@ public class List {
 			sb.append("Escolha uma opção: ");
 			System.out.println(sb);
 			int option = sc.nextInt();
-			
-			switch(option) {
-			case 1:// addBeginning
-				System.out.println("Which registration?");
-				int registration = sc.nextInt();
-				Node node = new Node(registration);
-				addBeginning(node);
-				break;
-			case 2:
-				
-				break;
-			case 3:
-				
-				break;
-			case 4:
-				
-				break;
-			case 5:
-				
-				break;
-			case 6:
-				print();
-				break;
-			case 7:
-				
-				break;
-			case 8:
-				
-				break;
-			case 9:
-				
-				break;
-			case 10:
-				
-				break;
-			case 11:
-				
-				break;
-			case 12:
-				
-				break;
-			case 13:
-				opcao = 13;
-				
-				break;
-			case 14:
+			try {
+				switch (option) {
+				case 1:// addBeginning
+					System.out.println("Which registration?");
+					int registration = sc.nextInt();
+					Node node = new Node(registration);
+					addBeginning(node);
+					break;
+				case 2:
+					System.out.println("Which registration?");
+					registration = sc.nextInt();
+					node = new Node(registration);
+					addMiddle(node);
+					break;
+				case 3:
+					System.out.println("Which registration?");
+					registration = sc.nextInt();
+					node = new Node(registration);
+					addTheEnd(node);
+					break;
+				case 4:
 
-				
-				break;
-			default:
-				System.out.println("Digito Inválido!");
+					break;
+				case 5:
+
+					break;
+				case 6:
+					print();
+					break;
+				case 7:
+
+					break;
+				case 8:
+
+					break;
+				case 9:
+
+					break;
+				case 10:
+
+					break;
+				case 11:
+
+					break;
+				case 12:
+
+					break;
+				case 13:
+					opcao = 13;
+
+					break;
+				case 14:
+
+					break;
+				default:
+					System.out.println("Invalid digit!");
+				}
+			} catch (DomainException a) {
+				System.out.println("Error: " + a.getMessage());
 			}
-		} while(opcao != 13);
+			
+		} while (opcao != 13);
 	}
-	
-	
 }
